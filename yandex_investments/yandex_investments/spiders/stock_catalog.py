be70f2dab6c5b74458e29553966449a55e338aa0
Denis Kindruk <thunder03026@gmail.com>
2021-12-09 16:14:53 +0300

    add spider

--- yandex_investments/yandex_investments/spiders/stock_catalog.py
+++ yandex_investments/yandex_investments/spiders/stock_catalog.py
@@ -1,10 +1,29 @@
 import scrapy
+from scrapy.item import Item, Field
+from yandex_investments.items import YandexInvestmentsStock
 
 
 class StockCatalogSpider(scrapy.Spider):
     name = 'stock_catalog'
-    allowed_domains = ['https://invest.yandex.ru/onboarding/']
-    start_urls = ['http://https://invest.yandex.ru/onboarding//']
+    start_urls = ['https://invest.yandex.ru/catalog/stock/']
 
-    def parse(self, response, **kwargs):
-        pass
+    def parse(self, response):
+        for stock in response.xpath(
+                "//a[@class='lBT6RuJRjtvzChEJ_G30 yDtbhftw9A4mXqH3vPkH ZPgoa_UnfWgowwkvsp64 N9w__d8iYPl6C0KewYiA']"):
+            item = YandexInvestmentsStock()
+            item['name'] = stock.xpath(".//div[@class='tJYWSxg23zNUv79hPvVS']/text()").get()
+            item['risk'] = stock.xpath(".//span[@class='oiTIR_Sf1_ObNUZhdLv5 mhSVC_KCosmKJ73T2s6O']/text()").get()
+            item['price'] = stock.xpath(".//div[@class='JGT__mSaFfXxcOb2oGto']/text()").get()
+            yield item
+
+# class StockCatalogSpider(scrapy.Spider):
+#     name = 'stock_catalog'
+#     start_urls = ['https://invest.yandex.ru/catalog/stock/']
+#
+#     def parse(self, response):
+#         for stock in response.xpath("//a[@class='lBT6RuJRjtvzChEJ_G30 yDtbhftw9A4mXqH3vPkH ZPgoa_UnfWgowwkvsp64 N9w__d8iYPl6C0KewYiA']"):
+#             yield {
+#                 'name': stock.xpath(".//div[@class='tJYWSxg23zNUv79hPvVS']/text()").get(),
+#                 'risk': stock.xpath(".//span[@class='oiTIR_Sf1_ObNUZhdLv5 mhSVC_KCosmKJ73T2s6O']/text()").get(),
+#                 'price': stock.xpath(".//div[@class='JGT__mSaFfXxcOb2oGto']/text()").get()
+#             }
