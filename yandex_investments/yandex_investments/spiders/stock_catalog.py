8ab5774f35609fcd4e39de2d43063da5ce338d24
Denis Kindruk <84288280+foreverYoungforeverDrunk@users.noreply.github.com>
2021-12-22 16:19:50 +0300

    Update stock_catalog.py

--- yandex_investments/yandex_investments/spiders/stock_catalog.py
+++ yandex_investments/yandex_investments/spiders/stock_catalog.py
@@ -15,15 +15,3 @@ class StockCatalogSpider(scrapy.Spider):
             item['risk'] = stock.xpath(".//span[@class='oiTIR_Sf1_ObNUZhdLv5 mhSVC_KCosmKJ73T2s6O']/text()").get()
             item['price'] = stock.xpath(".//div[@class='JGT__mSaFfXxcOb2oGto']/text()").get()
             yield item
-
-# class StockCatalogSpider(scrapy.Spider):
-#     name = 'stock_catalog'
-#     start_urls = ['https://invest.yandex.ru/catalog/stock/']
-#
-#     def parse(self, response):
-#         for stock in response.xpath("//a[@class='lBT6RuJRjtvzChEJ_G30 yDtbhftw9A4mXqH3vPkH ZPgoa_UnfWgowwkvsp64 N9w__d8iYPl6C0KewYiA']"):
-#             yield {
-#                 'name': stock.xpath(".//div[@class='tJYWSxg23zNUv79hPvVS']/text()").get(),
-#                 'risk': stock.xpath(".//span[@class='oiTIR_Sf1_ObNUZhdLv5 mhSVC_KCosmKJ73T2s6O']/text()").get(),
-#                 'price': stock.xpath(".//div[@class='JGT__mSaFfXxcOb2oGto']/text()").get()
-#             }
